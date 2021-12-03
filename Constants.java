17ab6039077fa4bae71b42a26db2bcfc644b95c0
Tomás <tomasg92@gmail.com>
2021-12-03 19:48:30 -0400

    Nueva ip para pruebas en Soprano y XPS

--- Constants.java
+++ Constants.java
@@ -2,7 +2,7 @@ public final class Constants {
   private Constants() {
   }
 
-  public static final String IP = "192.168.56.103";
+  public static final String IP = "192.168.0.113";
   // public static final String IP = "10.0.2.5";
   public static final String URL = "rmi://" + IP + "/adder";
 }
