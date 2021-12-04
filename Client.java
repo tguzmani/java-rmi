9e9cb12e7c819e47df6ea0b27965043e25d7dc8c
Tomás <tomasg92@gmail.com>
2021-12-03 20:59:15 -0400

    Ahora el cliente acepta entradas

--- Client.java
+++ Client.java
@@ -1,16 +1,24 @@
 import java.rmi.Naming;
+import java.util.Scanner;
 
 public class Client {
   public static void main(String[] args) {
-    int a = 10;
-    int b = 5;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("a = ");
+    int a = scanner.nextInt();
+
+    System.out.print("b = ");
+    int b = scanner.nextInt();
+
+    scanner.close();
 
     try {
       ICalculator serverSoprano = (ICalculator) Naming.lookup(Constants.URL_SOPRANO);
       ICalculator serverXPS = (ICalculator) Naming.lookup(Constants.URL_XPS);
 
-      System.out.println("[Soprano] 10 + 5 = " + serverSoprano.add(a, b));
-      System.out.println("[XPS] 10 * 5 = " + serverXPS.times(a, b));
+      System.out.println("[Soprano] " + a + " + " + b + " = " + serverSoprano.add(a, b));
+      System.out.println("[XPS] " + a + " * " + b + " = " + serverXPS.times(a, b));
 
     } catch (Exception e) {
       e.printStackTrace();
